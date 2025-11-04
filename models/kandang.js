b464d7a960c901d877a51f2d111f48aa821c4f73
aivylv <lyvialarasati@gmail.com>
2025-11-04 14:46:23 +0700

    membuat file kandang.js

---
+++ models/kandang.js
@@ -0,0 +1,32 @@
+module.exports  = (sequelize, DataTypes) => {
+    const Kandang = sequelize.define('Kandang', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nama_hewan: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        nama_petugas: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        usia_hewan: {
+            type: DataTypes.INTEGER
+        },
+        jenis_hewan: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        tahun_lahir: {
+            type: DataTypes.INTEGER
+        }
+    }, {
+        tableName: 'kandang',
+        timestamps: true,
+        freezeTableName: true
+    });
+    return Kandang;
+}
